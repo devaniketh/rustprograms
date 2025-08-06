bc5b5e4135539cd31a9632c32d1a4ee5809eb57c
aniketh <techrunstheworld@gmail.com>
2025-08-06 22:20:41 +0530

    fix:borrowers

--- practise/borrowers/target/debug/borrowers.d
+++
@@ -1,1 +0,0 @@
-/home/aniketh/projects/rust/practise/borrowers/target/debug/borrowers: /home/aniketh/projects/rust/practise/borrowers/src/main.rs
--- practise/borrowers/target/debug/deps/borrowers-943359d30cd464b7.d
+++
@@ -1,5 +0,0 @@
-/home/aniketh/projects/rust/practise/borrowers/target/debug/deps/borrowers-943359d30cd464b7.d: src/main.rs
-
-/home/aniketh/projects/rust/practise/borrowers/target/debug/deps/borrowers-943359d30cd464b7: src/main.rs
-
-src/main.rs:
